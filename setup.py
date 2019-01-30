e72af318c837621ab9852f3f6e1d6db8025025a0
Boris Savic <boris@olaii.com>
2019-01-30 10:41:02 +0100

    Upped version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='sepa_generator',
     packages=['sepa_generator'],
-    version='0.2.1',
+    version='0.2.2',
     description='Simple Python library for creating SEPA Credit Transfer XML documents',
     author='Boris Savic',
     author_email='boris70@gmail.com',
     url='https://github.com/boris-savic/python_sepa',
-    download_url='https://github.com/boris-savic/python_sepa/tarball/0.2.1',
+    download_url='https://github.com/boris-savic/python_sepa/tarball/0.2.2',
     keywords=['python sepa_generator generator', 'SEPA', 'SEPA Credit Transfer', 'SEPACreditTransfer'],
     classifiers=[],
     install_requires=[
